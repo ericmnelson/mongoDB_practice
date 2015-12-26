fdc5024690fd7cbcbfbc34323a675f57c8fe09d3
ericmnelson <ericmnelson.13@gmail.com>
2015-12-26 14:46:24 -0800

    Get requests working for single product as well as all

--- catalogAPI/app.js
+++ catalogAPI/app.js
@@ -1,8 +1,34 @@
 var express = require("express");
 var app = express();
+var mongojs = require("mongojs");
+var db = mongojs("catalog", ['products']);
 
 app.get("/", function(req, res){
 	res.send("It works!");
+})
+
+app.get("/products", function(req, res){
+	console.log("Fetching Products... ");
+	db.products.find(function(err, docs){
+		if(err){
+			res.send(err)
+		}else{
+			console.log("Sending Products...");
+			res.json(docs);
+		}
+	})
+})
+
+app.get("/products/:id", function(req, res){
+	console.log("Fetching Product w/ id: " + req.params.id);
+	db.products.findOne({_id:mongojs.ObjectId(req.params.id)}, function(err, doc){
+		if(err){
+			res.send(err)
+		}else{
+			console.log("Sending Product...");
+			res.json(doc);
+		}
+	})
 })
 
 app.listen(3000);
